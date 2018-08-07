23f7f7604c2c0acc4b51d1ddc4cd16d798d62093
Jeevan <jeevanfarias@gmail.com>
2018-08-06 22:27:50 -0400

    website strip down

--- main.js
+++ main.js
@@ -1,39 +1,16 @@
-var width = window.screen.width;
+window.onload = function(){
+	var title = document.getElementById("title");
+	var title_text = document.getElementById("title_text");
 
-$(document).ready( function() {
-		var elem = document.getElementById('draw-area');
-		var two = new Two({ fullscreen: true }).appendTo(elem);
-
-
-		var circle = two.makeCircle(- width/6, 0, 50);
-		var rect = two.makeRectangle(70, 0, 100, 100);
-		circle.fill = '#FF8000';
-		circle.stroke = 'orangered';
-		rect.fill = 'rgba(0, 200, 255, 0.75)';
-		rect.stroke = '#1C75BC';
-
-		// Groups can take an array of shapes and/or groups.
-		var group = two.makeGroup(circle, rect);
-
-		// And have translation, rotation, scale like all shapes.
-		group.translation.set(width / 2, two.height / 2);
-		group.rotation = Math.PI;
-		group.scale = 0.75;
-
-    two.bind('update', function(frameCount) {
-
-      if (group.scale > 0.9999) {
-          group.scale = group.rotation = 0;
-        }
-        var t = (1 - group.scale) * 0.125;
-        group.scale += t;
-        group.rotation += t * 4 * Math.PI;
-        group.translation.set(width/2, two.height/2);
-      }).play();
-
-});
-
-window.addEventListener('resize', function() {
-  width = window.innerWidth;
-  console.log(width);
-})
+	title.onmouseover = function(e){
+		console.log("hello")
+		var r = Math.floor(Math.random()*255);
+		var b = Math.floor(Math.random()*255);
+		var g = Math.floor(Math.random()*255);
+		console.log(r.toString(16)+b.toString(16)+g.toString(16));
+		title_text.setAttribute("style","color:#"+r.toString(16)+b.toString(16)+g.toString(16));
+	}
+	title.onmouseout = function(e){
+		title_text.setAttribute("style","color:#606060");
+	}
+}
